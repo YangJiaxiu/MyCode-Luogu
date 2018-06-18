341c61a02a06ec0898426e056322dbc255a5373d
YangJiaxiu <yjx.yjx@hotmail.com>
2018-06-18 23:33:23 +0800

    This code will be removed because that is very fuck, can't use set with class...

--- 1379.cpp
+++ 1379.cpp
@@ -1,42 +1,55 @@
 #include<iostream>
-#include<deque> 
-#include<string>
-#include<set>
+#include<deque>
+#include<unordered_set>
 
-struct Status {
-    std::string now;// 这个状态的“棋盘”
-    int step;// 当前的步数
-} start;
+const int MaxSize = 3 + 1;
+const int dx[] = {0, 1, 0, -1};
+const int dy[] = {1, 0, -1, 0};
+const int d[10] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
 
-std::multiset< std::string > Board;
+class Status {
+    public:
+        long long Board1;
+        short Board2[MaxSize][MaxSize];
+        int step = 0;
+        void Board_1To2() {
+            for (int i = 0; i < 9; ++i) {
+                Board1 += Board2[i / 3][i % 3] * d[i];
+            }
+        }
+        void Board_2To1() {
+            for (int i = 0; i < 9; ++i) {
+                Board2[i / 3][i % 3] = Board1 / d[i] % 10;
+            }   
+        }
+};
 
-int FindZero(const std::string &s) {
-    for (int i = 0; i < 9; ++i) if (s[i] == '0') return i;
-    return -1;
-}
+std::unordered_set< Status > isAppear;
 
-std::string NextBoard(std::string board, const int &way) {
-    int zero = FindZero(board);
-    if ((way == 0 && zero == 0) || (way == 1 && zero == 9 - 1)) return "failed";
-    (way == 0) ? std::swap(board[zero - 1], board[zero]) : std::swap(board[zero], board[zero + 1]);
-    return board;
-}
-
-int BFS() {
+int Bfs(const Status &first) {
     std::deque< Status > Q;
-    Q.push_back(start);
+    Q.push_back(first);
     while (!Q.empty()) {
-        Status root = Q.front();
-        Q.pop_front();
-        if (root.now == "123804765") return root.step;
-        for (int i = 0; i < 2; ++i) {
-            Status next;
-            next.now = NextBoard(root.now, i);
-            if (next.now == "failed" || Board.count(next.now)) continue;
-            next.step = root.step + 1;
-            Board.insert(next.now);
-            Q.push_back(next);
-            std::cerr << next.now + "\n";
+        Status start = Q.front(); Q.pop_front();
+        if (start.Board1 == 123804765LL) return start.step;
+        if (isAppear.count(start)) continue;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (start.Board2[i][j] == 0) {
+                    for (int way = 0; way < 4; ++way) {
+                        int nextX = i + dx[way], nextY = j + dy[way];
+                        if (nextX < 0 || nextY < 0 || nextX > 2 || nextY > 2) {
+                            continue;
+                        } else {
+                            Status next = start;
+                            next.step = start.step + 1;
+                            std::swap(next.Board2[i][j], next.Board2[nextX][nextY]);
+                            next.Board_2To1();
+                            Q.push_back(next);
+                        }
+                    }
+                }
+            }
         }
     }
     return -1;
@@ -44,8 +57,10 @@ int BFS() {
 
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cin >> start.now;
-    Board.insert(start.now);
-    std::cout << BFS();
+    Status first;
+    std::cin >> first.Board1;
+    first.Board_1To2();
+    std::cout << Bfs(first);
+    
     return 0;
 }
