c3a5a08a67aaad7d470cfcff8ae1068a46398da9
YangJiaxiu <yjx.yjx@hotmail.com>
2018-06-20 13:06:02 +0800

    This is a segment tree

--- SP1043.cpp
+++ SP1043.cpp
@@ -1,55 +1,92 @@
 #include<iostream>
 
-const int MaxN = 50000 + 10;
+const int MaxN = 500000 + 10;
 
-int N, M;
-
-struct FenwickTree {
-    int a[MaxN], C[MaxN];
-    void Init() {
-        memset(a, 0, sizeof(a));
-        memset(C, 0, sizeof(C));
+struct SegmentTree {
+    long long a[MaxN], tree[MaxN << 2], lazyTag[MaxN << 2];
+    inline int lson(const int &o) {
+        return o << 1;
     }
-
-    int LowBit(const int &x) {
-        return x & (-x);
+    inline int rson (const int &o) {
+        return o << 1 | 1;
     }
-
-    void Add(int x, int d) {
-        while (x <= N) {
-            C[x] += d;
-            x += LowBit(x);
+    inline void PushUp(const int &o) {
+        tree[o] = tree[lson(o)] + tree[rson(o)];
+    }
+    inline void Build(int o, int left, int right) {
+        lazyTag[o] = 0;
+        if (left == right) {
+            tree[o] = a[left];
+            return;
         }
+        int mid = (left + right) >> 1;
+        Build(lson(o), left, mid);
+        Build(rson(o), mid + 1, right);
+        PushUp(o);
+    }
+    inline void Mark(const int &o, int left, int right, long long k) {
+        lazyTag[o] += k;
+        tree[o] += k * (right - left + 1);
+    }
+    inline void PushDown(const int &o, int left, int right) {
+        int mid = (left + right) >> 1;
+        Mark(lson(o), left, mid, lazyTag[o]);
+        Mark(rson(o), mid + 1, right, lazyTag[o]);
+        lazyTag[o] = 0;
+    }
+    inline void Updata(const int &o, 
+                       const int &updataLeft, 
+                       const int &updataRight, 
+                       int left, 
+                       int right, 
+                       long long k) {
+        if (updataLeft <= left && updataRight >= right) {
+            lazyTag[o] += k;
+            tree[o] += k * (right - left + 1);
+            return;
+        }
+        int mid = (left + right) >> 1;
+        PushDown(o, left, right);
+        if (updataLeft <= mid) Updata(lson(o), updataLeft, updataRight, left, mid, k);
+        if (updataRight > mid) Updata(rson(o), updataLeft, updataRight, mid + 1, right, k);
+        PushUp(o);
     }
 
-    int Query(int x) {
-        int ans = 0;
-        while (x > 0) {
-            ans += C[x];
-            x -= LowBit(x);
-        }
+    inline long long Query(const int &o, 
+                           const int &queryLeft,
+                           const int &queryRight,
+                           int left,
+                           int right) {
+        
+        if (queryLeft <= left && queryRight >= right) return tree[o];
+        int mid = (left + right) >> 1;
+        long long ans = 0;
+        PushDown(o, left, right);
+        if (queryLeft <= mid) ans += Query(lson(o), queryLeft, queryRight, left, mid);
+        if (queryRight > mid) ans += Query(rson(o), queryLeft, queryRight, mid + 1, right);
         return ans;
     }
 } Tree;
 
-void debug1() {
-    for (int i = 0; i <= N; ++i) std::cerr << Tree.C[i] << "  ";
-}
-
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cin >> N;
-    Tree.Init();
-    for (int i = 1; i <= N; ++i) {
-        std::cin >> Tree.a[i];
-        Tree.Add(i, Tree.a[i]);
-    }
-    debug1();
-    std::cin >> M;
-    for (int i = 0; i < M; ++i) {
-        int x, y;
-        std::cin >> x >> y;
-        std::cout << Tree.Query(y)/* - Tree.Query(x - 1)*/ << "\n";
+    int N, M;
+    std::cin >> N >> M;
+    for (int i = 1; i <= N; ++i) std::cin >> Tree.a[i];
+    Tree.Build(1, 1, N);
+    
+    while (M--) {
+        int flag;
+        std::cin >> flag;
+        if (flag == 1) {
+            int x, y, k;
+            std::cin >> x >> y >> k;
+            Tree.Updata(1, x, y, 1, N, k);
+        } else {
+            int x, y;
+            std::cin >> x >> y;
+            std::cout << Tree.Query(1, x, y, 1, N) << "\n";
+        }
     }
     return 0;
 }
